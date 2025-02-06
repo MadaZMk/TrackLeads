5ea339a5e36e478d21ebafba1d30fe611fe75775
Magdalena Maciaszek <dth2@interia.pl>
2025-02-06 14:27:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let inputBtn = document.getElementById("saveBtn");
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked");
+});
+
+
+
+
+
+
+
+
+
+
+
+//netlify - platform to deploy websites - recommended to connect with github
+
